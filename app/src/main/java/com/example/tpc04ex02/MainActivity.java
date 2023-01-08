abb7d1aed269c4cfb0d37dafb4c3c88846795357
Oussama IOUIRY <121458608+Gl0rydayz@users.noreply.github.com>
2023-01-08 16:29:31 +0000

    modify: MainActivity.java-> Programmed the buttons responsible for changing the background color of the activity
    modify: colors.xml & main_activity.xml-> changed the color of a button

--- app/src/main/java/com/example/tpc04ex02/MainActivity.java
+++ app/src/main/java/com/example/tpc04ex02/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.tpc04ex02;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +13,8 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     EditText et_nombre;
     Button btn_reinitialiser, btn_quitter;
+    Button btn_left_color, btn_right_color, btn_middle_color;
+    ConstraintLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
         et_nombre = findViewById(R.id.et_nomber);
         btn_reinitialiser = findViewById(R.id.btn_reinitialiser);
         btn_quitter = findViewById(R.id.btn_quitter);
+        btn_left_color = findViewById(R.id.btn_left_color);
+        btn_right_color = findViewById(R.id.btn_right_color);
+        btn_middle_color = findViewById(R.id.btn_middle_color);
+        layout = findViewById(R.id.c_layout);
 
         btn_reinitialiser.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +43,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        btn_left_color.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout.setBackgroundResource(R.color.btn_left_color);
+            }
+        });
+
+        btn_right_color.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout.setBackgroundResource(R.color.btn_right_color);
+            }
+        });
+
+        btn_middle_color.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                layout.setBackgroundResource(R.color.btn_middle_color);
             }
         });
     }
