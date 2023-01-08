5b3f0aa45eebabbc956cbd2c9018cb3a1b2e42b9
Oussama IOUIRY <121458608+Gl0rydayz@users.noreply.github.com>
2023-01-08 09:12:09 +0000

    modify: MainActivity.java-> Button REINITIALISER is programmed to clear the EditText
    modify: MainActivity.java-> Button QUITTER is programmed to quit the application

--- app/src/main/java/com/example/tpc04ex02/MainActivity.java
+++ app/src/main/java/com/example/tpc04ex02/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.tpc04ex02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText et_nombre;
+    Button btn_reinitialiser, btn_quitter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        et_nombre = findViewById(R.id.et_nomber);
+        btn_reinitialiser = findViewById(R.id.btn_reinitialiser);
+        btn_quitter = findViewById(R.id.btn_quitter);
+
+        btn_reinitialiser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                et_nombre.getText().clear();
+            }
+        });
+
+        btn_quitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
