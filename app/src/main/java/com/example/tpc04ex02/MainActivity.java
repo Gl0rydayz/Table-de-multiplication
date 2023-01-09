bb6f9bf69c009a3e30fa467fcf70f394ba31993e
Oussama IOUIRY <121458608+Gl0rydayz@users.noreply.github.com>
2023-01-09 09:02:10 +0000

    modify: MainActivity.java-> Button AFFICHER is programmed to show the multiplication table
    modify: main_activity.xml-> added a maxLines property

--- app/src/main/java/com/example/tpc04ex02/MainActivity.java
+++ app/src/main/java/com/example/tpc04ex02/MainActivity.java
@@ -7,10 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     EditText et_nombre;
     Button btn_reinitialiser, btn_quitter;
+    Button btn_afficher;
+    TextView tv_table;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         et_nombre = findViewById(R.id.et_nomber);
         btn_reinitialiser = findViewById(R.id.btn_reinitialiser);
         btn_quitter = findViewById(R.id.btn_quitter);
+        btn_afficher = findViewById(R.id.btn_afficher);
+        tv_table = findViewById(R.id.tv_table);
 
         btn_reinitialiser.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +40,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        btn_afficher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv_table.setText("");
+                int i = 0;
+                while (i <= tv_table.getMaxLines()) {
+                    int res = Integer.parseInt(et_nombre.getText().toString()) * i;
+                    String tableMultiplication = et_nombre.getText().toString() + " * " + i + " = " + res + "\n";
+                    tv_table.append(tableMultiplication);
+                    i++;
+                }
             }
         });
     }
