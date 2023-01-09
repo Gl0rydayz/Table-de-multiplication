af42f7a40e66b0bb1861e94a3d917fb626fe85cd
Oussama IOUIRY <121458608+Gl0rydayz@users.noreply.github.com>
2023-01-09 14:02:59 +0000

    modify: MainActivity.java-> handled the empty inputs

--- app/src/main/java/com/example/tpc04ex02/MainActivity.java
+++ app/src/main/java/com/example/tpc04ex02/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText et_nombre;
@@ -46,13 +47,24 @@ public class MainActivity extends AppCompatActivity {
         btn_afficher.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                tv_table.setText("");
-                int i = 0;
-                while (i <= tv_table.getMaxLines()) {
-                    int res = Integer.parseInt(et_nombre.getText().toString()) * i;
-                    String tableMultiplication = et_nombre.getText().toString() + " * " + i + " = " + res + "\n";
-                    tv_table.append(tableMultiplication);
-                    i++;
+                try {
+                    if (!et_nombre.getText().toString().equals("")) {
+                        tv_table.setText("");
+                        int i = 0;
+                        while (i <= tv_table.getMaxLines()) {
+                            int res = Integer.parseInt(et_nombre.getText().toString()) * i;
+                            String tableMultiplication = et_nombre.getText().toString() + " * " + i + " = " + res + "\n";
+                            tv_table.append(tableMultiplication);
+                            i++;
+                        }
+                    } else {
+                        Intent intent = getIntent();
+                        finish();
+                        startActivity(intent);
+                        throw new NumberFormatException();
+                    }
+                } catch (NumberFormatException e) {
+                    Toast.makeText(MainActivity.this, "Veuillez saisir un entier!!!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
